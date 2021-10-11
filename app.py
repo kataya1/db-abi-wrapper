e7d932b114ddb49bc8768418deac2bfcbc5d5966
kataya1 <mohamedkataya1@gmail.com>
2021-10-11 22:02:52 +0200

    shouldn't have spend all this time on this shit

--- app.py
+++ app.py
@@ -1,36 +1,43 @@
-from model import table
+from model import table, db
 class employee:
-    employee_list = []
-    
+    employee_list = {}
+    table_emp = None
     def __init__(self,first_name, last_name, DOB, department, salary):
         self.first_name =first_name
         self.last_name = last_name
         self.DOB = DOB 
         self.department = department
         self.salary = salary
-        employee.employee_list.append(self)
+
         # TODO ■ Insert new record in table employee in database
+        employee.table_emp.insert(**{'first_name':first_name, 'last_name':last_name, 'dob':DOB, 'department': department, 'salary':salary})
+        res = employee.table_emp.read(f'select id from employee order by id desc limit 1;')
+        self.id = res[0][0]
+        employee.employee_list[self.id] = self
 
     def transfer(self,new_dept):
         self.department = new_dept
         # TODO Update the database record with the update
+        employee.table_emp.update( **{'department':new_dept}, where=f'id = {self.id}')
 
     def fire(self):
-        employee.employee_list.remove(self)
+        del employee.employee_list[self.id]
         # TODO
         # ■ Delete its record from the database
+        employee.table_emp.delete(where=f'id = {self.id}')
+
     
     def __str__(self):
-        return f'name: {self.first_name} {self.last_name}, DOB: {self.DOB}, department: {self.department}, salary: {self.salary}'
+        return f'id: {self.id}, name: {self.first_name} {self.last_name}, DOB: {self.DOB}, department: {self.department}, salary: {self.salary}'
 
     def show(self):
         print(self)
     
     @staticmethod
     def list_employees():
-        for emp in employee.employee_list:
+        for emp in employee.employee_list.values():
             print(emp)
-        
+    
     # there should be a department table  
     # there shouldn't be a manager table because a manager is an employee and he manages a dept in a 1 to 1 may-must relationship. 
     # the department table should have the manager id.
@@ -38,26 +45,96 @@ class employee:
     # just work with this badly designed database for now man. i wanna finish and sleep
 
 class manager(employee):
-
     def __init__(self,first_name, last_name, DOB, department, salary, managed_department):
         super().__init__(first_name, last_name, DOB, department, salary)
         self.managed_department = managed_department
 
+
+    
+
     def __str__(self):
-        return f'name: {self.first_name} {self.last_name}, DOB: {self.DOB}, department: {self.department}, salary: "classified"'
+        return f'id: {self.id}, name: {self.first_name} {self.last_name}, DOB: {self.DOB}, department: {self.department}, managed department: {self.managed_department}, salary: "classified"'
+
+def setup():
+    dbobj = db(database='oop_psycopg2', user='postgres', password='postgres',host='127.0.0.1', port='5432')
+    dbobj.reset_db()
+
+    department_table_properties = {
+        'columns': {
+                    'name': 'text primary key',
+                    'manager_id': 'int'
+        },
+        'table_constraints': []
+    }
+    emp_table_properties = {
+        'columns': {
+                    'id': 'serial primary key',
+                    'first_name':'text not null',
+                    'last_name':'text',
+                    'DOB': 'date',
+                    'department': 'varchar(50)',
+                    'salary': 'numeric(10,3)'
+                    },
+        'table_constraints': []
+    }
+    
+        # 'table_constraints': ['foreign key (department) references department(name)'] #
+
+    table_department = table('department',dbobj, 2, department_table_properties)
+    emp_t = table('employee',dbobj, 6, emp_table_properties)
+    # table_department.alter('alter table department add constraint fk_fk foreign key (manager_id) references employee(id);')
+    employee.table_emp = emp_t
+    employee('john', 'doe', '2-5-1978', 'Accounting', 30000)
+    manager('jane', 'doe', '4-9-1944', 'Management', 230000.22, 'Accounting')
+
+def main():
+    def hire():
+        w = input('\nso you wanna hire!, Ok\t e for employee, m for manager, what\'s it gonna be? ')
+        print('Employee:' if w == 'e' else ('Manager:' if w == 'm' else 'what\'s that!? bye!'))
+        inp = {'first_name': '', 'last_name': '', 'DOB': 'format(dd-mm-yyyy)', 'department': '', 'salary': ''}
+        for key, value in inp.items():
+            inp[key] = input(f'please enter {key} {value}:')
+        inp['salary'] = int(inp['salary'])
+        if w == 'm':
+            inp['managed_department'] = input('please enter managed department? ')
+            manager(**inp)
+        elif w == 'e':
+            employee(**inp)
         
+    def transfer():
+        eid = input('id? ')
+        employee.employee_list[int(eid)].transfer(input('new department? '))
+    def listemp():
+        employee.list_employees()
+    def fire():
+        employee.employee_list[int(input('input employee id: '))].fire()
+    def quitgame():
+        return 1
 
-
+    menu ={
+        'hire': hire,
+        'transfer': transfer,
+        'fire': fire,
+        'list': listemp,
+        'q': quitgame,
+        
+    }
+    def game():
+        print('hello there\nhere is what you can do:\n\t\t\t\t\t\tkeywords\n"hire": hire new employee enter\t"transfer": transfer employee to another department\t"fire": you know what it does\t"list": list info about all employees')
+        userin = input('\nso what\'re you gonna do? ')
+        if userin in menu:
+            quit_flag = menu[userin]()
+        if quit_flag == 1:
+            print('bye')
+            return
+        else:
+            print("-------------------------------------------\n")
+            return game()
+    game()
 
 if __name__ == '__main__':
-    dbobj = table.db(database='oop_psycopg2', user='postgres', password='postgres',host='127.0.0.1', port='5432')
-    dbobj.initialize_db()
-    emp1 = employee('john','doe','1-5-1984','Accounting', 30000)
-    mang1 = manager('jane', 'doe', '6-6-1965', 'Management', 150000, 'Accounting')
-
-    # emp1.show()
-    # mang1.show()
- 
-
-    mang1.list_employees()
-    # manager.list_employees()
+    try:
+        setup()
+        main()
+    except Exception as e:
+        print(e)
--- model.py
+++ model.py
@@ -4,11 +4,10 @@
 #                        host='127.0.0.1', port='5432')
 
 # print('database connection successful 💪')
-
-
 import psycopg2
 import functools
 
+# TODO f string escaping
 class db:
     def __init__(self, database, user, password, host, port):
         self.database = database
@@ -26,15 +25,16 @@ class db:
                 cursor = con.cursor()
                 # print('args in wrapper ',args)
                 # print('kwargs in wrapper ',kwargs)
-                func(cur=cursor,*args,**kwargs)
+                ret = func(cur=cursor,*args,**kwargs)
                 con.commit()
+                return ret
                 # maybe passing cur=currsor like that isn't that best way
                 # args[0].__setattr__('cur',cursor); func(*args, **kwargs) #where args is the obj(self) #wont work on a class method
                 #     or
                 # wrapper(obj,*args,**kwargs); obj.cur = cursor; func(obj,*args,**kwargs); self.cur = 
                 # --------
             except Exception as e:
-                print(f'⚠⚠⚠ error: {e}')
+                print(f'⚠⚠⚠ function: {func.__name__} | error: {e}')
                 con.rollback()
             finally:
                 con.close()
@@ -42,22 +42,32 @@ class db:
 
     def initialize_db(self, sql):
         con = psycopg2.connect(database=self.database, user=self.user, password=self.password, host=self.host, port=self.port)
+        # read all before commiting i think instead of auto commiting
         con.set_isolation_level(2)
         cursor = con.cursor()
         cursor.execute(sql)
         # cursor.execute('alter table department add constraint fk_fk foreign key (manager_id) references employee(id);')
         con.commit()
         con.close()
+
+    # @connect nvm this will need yet another wrapper ()
+    def reset_db(self, cur=None):
+        con = psycopg2.connect(database='postgres', user=self.user, password=self.password, host=self.host, port=self.port)
+        con.set_isolation_level(0)
+        cur = con.cursor()
+        cur.execute(f'drop database if exists {self.database};')
+        cur.execute(f'create database {self.database};')
+        
+        con.close()
             
 
 class table:
-    def __init__(self, table_name, db__obj, num_columns, **kwargs):
+    def __init__(self, table_name='', db__obj=None, num_columns=0, table_properties={}, create=True):
         self.table_name = table_name
-
-        # table.db_obj = db__obj
+        # day3: why does a table need a db object if it only uses it once #too late to think about it anyway  
         self.db_obj = db__obj
         self.num_columns = num_columns
-        self.table_properties = kwargs
+        self.table_properties = table_properties
         '''
         for key, value in kwargs.items():
             # print(f'kwargs ---> {key}, {value}')
@@ -65,86 +75,93 @@ class table:
             # self.(key) = value
             # TODO when you pass a column as kwargs maybe the text will be forignkey table_name(col) filter this to be added on a separate row. maybe primary key too
         ''' # this was cool but it's not the right way
-        # self.__create()
+        if create:
+            self.__create()
 
-    # what the heck did i creat what is this!?? why is this!?? (double double wrapper) 
-    def connect(func):
+    # what the heck did i create🙃 what is this😭!?? why is this🥶!?? 
+    def tb_connect(func):
         @functools.wraps(func)
         def wrapper(self, *args,**kwargs):
             # why does it know db_obj now ? #ans it was getting the global object defined down with the same variable name so i change it down there
-            self.db_obj.connect(func)(self, *args,**kwargs)
+            # self here is the table instance, it gets passe into args over there at db
+            return self.db_obj.connect(func)(self, *args,**kwargs)
         return wrapper
 
     # maybe this whole function is stupid i don't remember why i created it this way
-    # @classmethod #how does a class access the attributes of the instance? #ans: well it gonna be called in an instance method pass the self.vars to it #nvm 
-    # @connect
-    # @db_obj.connect #why does it notw kno db_obj
+    # @classmethod #how does a class access the attributes of the instance? #ans: well it's gonna be called in an instance method pass the self.vars to it #nvm 
+    # @db_obj.connect #why does it not know db_obj
+    @tb_connect
     def __create(self, cur=None):
         'create a new table. an instance dont create anything only the class does when instantiating'
 
         if len(self.table_properties['columns']) != self.num_columns:
             raise Exception('number of columns specified and len of columns in table_properties doesn\'t match')
-        cur.execute(f'''
-        create table if not exists {self.table_name}(
-            {', '.join([f'{k} {v}' for k,v in self.table_properties['columns'].items()])},
-            {', '.join(self.table_properties['table_constraints'])}
-        );''')
-
+        # cur.execute(f'''
+        # create table if not exists {self.table_name}(
+        #     {', '.join([f'{k} {v}' for k,v in self.table_properties['columns'].items()])},
+        #     {', '.join(self.table_properties['table_constraints'])}
+        # );''')
+        cur.execute(self.formulate_table(self.table_name, self.table_properties))
         
     # CRUD
-    @connect
+    @tb_connect
     def insert(self, cur=None, **kwargs ):
         'insert a record into the table'
-        # print(','.join(kwargs.keys()))
-        # print(kwargs)
-        # print(kwargs)
-        # print(','.join(kwargs.keys()))
-        # print(kwargs.values())
-        # print(','.join( [ (f" '{v}' " if type(v) == str else f" {v} ") for v in kwargs.values() ] ))
-        
         cur.execute(f'''insert into {self.table_name}({','.join(kwargs.keys())}) values({','.join( [ (f" '{v}' " if type(v) == str else f" {v} ") for v in kwargs.values() ] )});''')
 
-    @connect
+    @tb_connect
     def read(self,query, cur=None):
         'a select statement'
         cur.execute(query)
-        for emp in cur.fetchall():
-            print(emp)
-        print('-------')
+        res = cur.fetchall()
+        # for emp in res:
+        #     print(emp)
+        # print('-------')
+        return res
         
-    @connect
+    @tb_connect
     def update(self, cur=None, where = '',**kwargs):
         'update the volues of a column in the table(NOT IMPLEMENTED)'
         setters = ','.join([f"{key} = '{value}'" for key, value in kwargs.items()])
         cur.execute(f"update {self.table_name} set {setters} {f'where  {where}' if where else '' };")
 
-    @connect
+    @tb_connect
     def delete(self, cur=None, where = ''):
         'drop row(IN TABLE)'
         cur.execute(f"delete from {self.table_name} {f'where  {where}' if where else '' };")
 
-        
-    @connect
+    @tb_connect
     def alter(self, query, cur=None):
         'change table add/drop/modify column(NOT IMPLEMENTED'
-        cur.execute(query)
+        cur.execute(query) #lazy implementation
 
-    @connect
+    @tb_connect
     def drop(self, cur=None, options=''):
         'drop table'
         cur.execute(f'drop table if exists {self.table_name} {options};')
-
+    
+    @classmethod
+    @tb_connect
+    def drop(cls, table_name, cur=None, options=''):
+        'drop table'
+        cur.execute(f'drop table if exists {table_name} {options};')
     
     # cur is passed from the wraper functtion
-    @connect
+    @tb_connect
     def truncate(self,options='',cur=None):
         'remove all entries from table'
-        #we only havea one statement related to the functionality that means it's time for a wrapper/ decorator
         cur.execute(f'truncate table {self.table_name} {options};') 
 
+    @staticmethod
+    def formulate_table(table_name, table_properties):
+        'returns a create table sql statement from the'
+        return f'''
+        create table if not exists {table_name}(
+            {', '.join([f'{k} {v}' for k,v in table_properties['columns'].items()])}
+            {',' if table_properties['table_constraints'] else ''}
+            {', '.join(table_properties['table_constraints'])}
+        );'''
     
-
-
 if __name__ == "__main__":
 
     emp_table_properties = {
@@ -153,53 +170,69 @@ if __name__ == "__main__":
                     'first_name':'text not null',
                     'last_name':'text',
                     'DOB': 'date',
-                    'departement': 'varchar(50) null',
+                    'department': 'varchar(50)',
                     'salary': 'numeric(10,3)'
                     },
-        'table_constraints': ['foreign key (department) references department(name) INITIALLY DEFERRED']
+        'table_constraints': ['foreign key (department) references department(name)']
     }
 
     department_table_properties = {
         'columns': {
                     'name': 'text primary key',
-                    'manager_id': 'int null'
+                    'manager_id': 'int'
         },
-        'table_constraints': ['foreign key (manager_id) references employee(id) INITIALLY DEFERRED']
+        'table_constraints': []
     }
-    try:
-        dbobj = db(database='oop_psycopg2', user='postgres', password='postgres',host='127.0.0.1', port='5432')
-        dbobj.initialize_db( '''
-                create table if not exists department(
-                        name text primary key,
-                        manager_id int
-                    );
-                create table if not exists employee(
-                        id serial primary key,
-                        first_name text not null,
-                        last_name text,
-                        DOB date,
-                        department varchar(50) ,
-                        salary numeric(10,3),
-                        foreign key (department) references department(name));''' )
-                        
-        department_table = table('department',dbobj, 2, **department_table_properties)
-        emp_t = table('employee',dbobj, 6, **emp_table_properties)
-        emp_t.truncate()
-        emp_t.insert(**{'first_name':'jimmy', 'dob':'4-5-1987', 'salary':37888})
-        emp_t.insert(**{'first_name':'jane', 'dob':'4-5-1907', 'salary':37388})
-        emp_t.insert(**{'first_name':'john', 'dob':'4-3-1977', 'salary':50008})
-        emp_t.insert(**{'first_name':'falco', 'dob':'4-5-1957', 'salary':37888})
-        emp_t.insert(**{'first_name':'reyner', 'dob':'4-5-1887', 'salary':37888.8}, last_name='fred')
-        emp_t.read('select * from employee')
-        emp_t.update(**({'last_name':'neutron', 'salary': 100000}) , where="first_name = 'jimmy'" )
-        emp_t.delete(where= "first_name = 'john'")
-        emp_t.read('select * from employee')
-        # emp_t.read('select * from employee')
+        # 'table_constraints': ['foreign key (manager_id) references employee(id)']
 
+    
+    dbobj = db(database='oop_psycopg2', user='postgres', password='postgres',host='127.0.0.1', port='5432')
+    dbobj.reset_db()
+    
+    # dbobj.initialize_db( '''
+    #         create table if not exists department(
+    #                 name text primary key,
+    #                 manager_id int
+    #             );
+    #         create table if not exists employee(
+    #                 id serial primary key,
+    #                 first_name text not null,
+    #                 last_name text,
+    #                 DOB date,
+    #                 department varchar(50) ,
+    #                 salary numeric(10,3),
+    #                 foreign key (department) references department(name));
+    #         alter table department add constraint fk_fk foreign key (manager_id) references employee(id);
+    #         ''' )
+    # print(f'''
+    #     {table.formulate_table('department', department_table_properties)}
+    #     {table.formulate_table('employee', emp_table_properties)}
+    #     alter table department add constraint fk_fk foreign key (manager_id) references employee(id);
+    # ''')
+    # dbobj.initialize_db(f'''
+    #     {table.formulate_table('department', department_table_properties)}
+    #     {table.formulate_table('employee', emp_table_properties)}
+    # ''')
+        # alter table department add constraint fk_fk foreign key (manager_id) references employee(id);
+    
+    # day3: we have literaly come full circle
+    # d = table(db__obj=dbobj, create=False)
+    # d.drop('employee', options='cascade')
+    # d.drop('department', options='cascade')
 
-    except Exception as e:
-        print(e)
-
+    department_table = table('department',dbobj, 2, department_table_properties)
+    emp_t = table('employee',dbobj, 6, emp_table_properties)
+    department_table.alter('alter table department add constraint fk_fk foreign key (manager_id) references employee(id);')
+    # emp_t.truncate(options='cascade')
+    emp_t.insert(**{'first_name':'jimmy', 'dob':'4-5-1987', 'salary':37888})
+    emp_t.insert(**{'first_name':'jane', 'dob':'4-5-1907', 'salary':37388})
+    emp_t.insert(**{'first_name':'john', 'dob':'4-3-1977', 'salary':50008})
+    emp_t.insert(**{'first_name':'falco', 'dob':'4-5-1957', 'salary':37888})
+    emp_t.insert(**{'first_name':'reyner', 'dob':'4-5-1887', 'salary':37888.8}, last_name='fred')
+    emp_t.read('select * from employee')
+    emp_t.update(**({'last_name':'neutron', 'salary': 100000}) , where="first_name = 'jimmy'" )
+    emp_t.delete(where= "first_name = 'john'")
+    emp_t.read('select * from employee')
 
 '''
 create table if not exists employee(
